acf8536efb54b935f4e5ac736af33bfff158669f
Dmitry Vasnetsov <76834554+dvasnetsov@users.noreply.github.com>
2025-11-01 02:32:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  // ВАЖНО: замени на имя своего репозитория, например "/samocvety-manager/"
   base: "/MANAGER-TEST/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
